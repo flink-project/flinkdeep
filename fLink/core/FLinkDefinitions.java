5b81ff6a4bd0ad890e6c61a6d0b95e2a61adbd07
MarcelGehrig <marcel.gehrig@ntb.ch>
2016-02-15 12:14:08 +0100

    "FLinkDefinitions.java" is now automatically created with "flinkinterface"

--- fLink/core/FLinkDefinitions.java
+++ fLink/core/FLinkDefinitions.java
@@ -1,3 +1,23 @@
+/** ***************************************************************************
+**  _________     _____      _____    ____  _____    ___  ____               **
+** |_   ___  |  |_   _|     |_   _|  |_   \|_   _|  |_  ||_  _|              **
+**   | |_  \_|    | |         | |      |   \ | |      | |_/ /                **
+**   |  _|        | |   _     | |      | |\ \| |      |  __'.                **
+**  _| |_        _| |__/ |   _| |_    _| |_\   |_    _| |  \ \_              **
+** |_____|      |________|  |_____|  |_____|\____|  |____||____|             **
+**                                                                           **
+*******************************************************************************
+**                                                                           **
+** fLink definitions                                                         **
+**                                                                           **
+**  THIS FILE WAS CREATED AUTOMATICALLY - do not change                      **
+**                                                                           **
+**  Created with: flinkinterface/func_id/                                    **
+**                   create_FLinkDefinitions.java_flinkdeep.sh               **
+**                                                                           **
+*******************************************************************************
+*/
+
 package fLink.core;
 
 public interface FLinkDefinitions {
@@ -17,17 +37,17 @@ public interface FLinkDefinitions {
 	public static final int MOD_CONF_OFFSET = 0x14;
 	
 	
-	public static final int INFO_DEVICE_ID				=	0x0;	
-	public static final int PWM_INTERFACE_ID			=	0xC;	
-	public static final int GPIO_INTERFACE_ID			=	0x5;
-	public static final int COUNTER_INTERFACE_ID		=	0x6;	
-	public static final int WD_INTERFACE_ID				=	0x10;	
-	public static final int PPWA_INTERFACE_ID			=	0xD;	
-	public static final int ANALOG_INPUT_INTERFACE_ID 	= 	0x1;
-	public static final int ANALOG_OUTPUT_INTERFACE_ID 	= 	0x2;
-	
-	public static final int INTERFACE_TYPE_MASK = 0xFFFF;
-	public static final int INFO_DEVICE_SIZE = 0x80;
+	public static final int INFO_DEVICE_ID			=	0x00;
+	public static final int ANALOG_INPUT_INTERFACE_ID			=	0x01;
+	public static final int ANALOG_OUTPUT_INTERFACE_ID			=	0x02;
+	public static final int GPIO_INTERFACE_ID			=	0x05;
+	public static final int COUNTER_INTERFACE_ID			=	0x06;
+	public static final int PWM_INTERFACE_ID			=	0x0C;
+	public static final int PPWA_INTERFACE_ID			=	0x0D;
+	public static final int WD_INTERFACE_ID			=	0x10;
+ 
+ 	public static final int INTERFACE_TYPE_MASK = 0xFFFF;
+ 	public static final int INFO_DEVICE_SIZE = 0x80;
 
 
-}
+ }
